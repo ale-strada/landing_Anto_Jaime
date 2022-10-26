fb5c6df249794575457180c4a8661f4676f7f005
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-10-26 12:01:41 -0300

    section contacto

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//import Swal from "sweetalert2/dist/sweetalert2.js";
+
 //maneja el scroll del header
 window.addEventListener("scroll", function () {
   var header = document.querySelector("header");
@@ -18,9 +20,73 @@ buttonProyects.addEventListener("click", (e) => {
   console.log("PROYECTOS");
 });
 
+//boton para descargar CV
 const downloadCv = document.querySelector(".download-cv");
 downloadCv.addEventListener("click", (e) => {
   e.preventDefault();
   window.open("CURRICULUM ANTONELLA JAIME DG.pdf");
   console.log("DESCARGA CV");
+});
+async function sendEmail(mensaje) {
+  console.log("envia");
+  try {
+    const res = await fetch("https://dwf-m7-final.herokuapp.com/send", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        to: "jaime.antonella29@gmail.com",
+        from: "buscador.de.mascotas.app@gmail.com",
+        subject: "mensaje landing",
+        text: mensaje,
+      }),
+    });
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//manda el email con l API findPets
+async function sendEmail(mensaje) {
+  const res = await fetch("https://dwf-m7-final.herokuapp.com/send", {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      to: "jaime.antonella29@gmail.com",
+      from: "buscador.de.mascotas.app@gmail.com",
+      subject: "mensaje desde landing page",
+      text: mensaje,
+    }),
+  });
+  const data = await res.json();
+
+  return data;
+}
+
+// formulario de contacto
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".contacto_form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const mensaje = {
+      nombre: e.target.name.value,
+      email: e.target.email.value,
+      mensaje: e.target.mensaje.value,
+    };
+    const email = sendEmail(JSON.stringify(mensaje));
+    console.log(email);
+    Swal.fire({
+      title: "Su mensaje fue enviado",
+      icon: "success",
+      confirmButtonText: "ok",
+    });
+    form.reset();
+  });
 });
